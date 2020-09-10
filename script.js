47f7a1aa74194fda876234c6832b42e922339c1c
GameDog9988 <51346343+GameDog9988@users.noreply.github.com>
2020-09-10 15:31:57 -0400

    Async Await

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const img = document.querySelector("img");
-fetch(
-  "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=TG8j88CyB2aX4eKpQcJYN5riPNjQvgRa&limit=5",
-  { mode: "cors" }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data[0].images.original.url;
-  });
+
+async function getRyan() {
+  const response = await fetch(
+    "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=TG8j88CyB2aX4eKpQcJYN5riPNjQvgRa&limit=5",
+    { mode: "cors" }
+  );
+  const ryanData = await response.json();
+  img.src = ryanData.data[0].images.original.url;
+}
+
+getRyan();
